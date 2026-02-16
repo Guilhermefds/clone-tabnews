f784e91a8d7cda352b8e55b465f3be0877731264
Guilhermefds <guiferreira.sma@gmail.com>
2026-02-16 16:51:58 -0300

    adicionando o export default que faltu

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-function Home() {
-    return <h1>Oi, Meu Amor! Te Amo! s2; Você é linda demais!</h1>;
-    }
- 
+export default function Home() {
+  return <h1>Oi, Meu Amor! Te Amo! s2; Você é linda demais!</h1>;
+}
