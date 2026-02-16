27d903831143a16f9ee5954ac5f2545823893633
Guilhermefds <guiferreira.sma@gmail.com>
2026-02-16 18:09:30 -0300

    Adicionando mais uma frase

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>Oi, Meu Amor! Te Amo! s2; Você é linda demais!</h1>;
+  return <h1>Oi, Meu Amor! Te Amo! s2; Você é linda demais! Estudar é bem legal!</h1>;
 }
